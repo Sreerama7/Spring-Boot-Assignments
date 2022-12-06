0a7d7c5f736435797d0f73fa8193a2754afc25c3
Sreerama7 <47537946+Sreerama7@users.noreply.github.com>
2022-12-06 10:56:42 +0530

    Create HashPassword.java

---
+++ SOLID PRINCIPLES/Open Closed Principle/Without Principle/HashPassword.java
@@ -0,0 +1,17 @@
+public class HashPassword{
+  
+
+ public String hashPassword(String password, HashingType hashingType)
+{
+    if(HashingType.BASE64.equals(hashingType))
+    {
+        hashedPassword="hashed with Base64";
+    }
+    else if(HashingType.MD5.equals(hashingType))
+    {
+        hashedPassword="hashed with MD5";
+    }
+
+    return hashedPassword;
+}
+}
